3c33422e4a969447a774b8af6577c13bc1f1943d
Roelof Pieters <graphific@gmail.com>
2025-03-04 20:27:12 +0100

    gh pages static build

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/guardiao/' : '',
+  basePath: isProd ? '/guardiao' : '',
+  output: 'export'
+};
+
+export default nextConfig;
