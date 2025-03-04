54d92dc91357405eb52381099ac268d6c16dd450
Roelof Pieters <graphific@gmail.com>
2025-03-04 20:36:08 +0100

    Updates

---
+++ _next/static/chunks/app/layout-88e375bb7582b013.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5299:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_aba6aa"}},6105:(e,a,s)=>{Promise.resolve().then(s.t.bind(s,5299,23)),Promise.resolve().then(s.t.bind(s,9324,23))},9324:()=>{}},e=>{var a=a=>e(e.s=a);e.O(0,[618,441,684,358],()=>a(6105)),_N_E=e.O()}]);
